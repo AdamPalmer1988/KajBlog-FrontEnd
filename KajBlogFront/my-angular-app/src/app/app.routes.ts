81a249e4137e2aee80a9f6cac6a28f4f673fda5d
Koren P <wisepixie00@gmail.com>
2024-10-13 15:34:25 -0400

    bare bones styling of form and styles.css, need to figure out how to fit the form middle of page with no overhang on top/bottom. Still need to make buttons 'work' and add a posted field/favorites section

--- KajBlogFront/my-angular-app/src/app/app.routes.ts
+++ KajBlogFront/my-angular-app/src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { BlogFormComponent } from './components/blog-form/blog-form.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { FavoritesListComponent } from './components/favorites-list/favorites-list.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const routes: Routes = [
     {path: '', component:MainPageComponent},
     {path: '', component:BlogFormComponent},
     {path: '', component:CategoryListComponent},
     {path: '', component:FavoritesListComponent},
-    {path: '', component:NavBarComponent}
+    {path: '', component:NavBarComponent},
+    {path: '', component:PostListComponent},
+    {path: '', component:ReactiveFormsModule}
 ];
 
 
--- KajBlogFront/my-angular-app/src/app/components/blog-form/blog-form.component.ts
+++ KajBlogFront/my-angular-app/src/app/components/blog-form/blog-form.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-blog-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './blog-form.component.html',
   styleUrl: './blog-form.component.css'
 })
 export class BlogFormComponent {
 
+  postForm!: FormGroup;
+onSubmit: any;
+myForm: any;
+
+  constructor(private fb: FormBuilder,
+    private router: Router){}
+
+
+    ngOnInit(){
+      this.postForm = this.fb.group({
+        subject: ['', Validators.required],
+        body: ['', Validators.required, Validators.maxLength(10000)],
+       // img: ['', Validators.required],
+        userName: ['', Validators.required]
+      })
+    }
 }
