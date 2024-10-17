7733e9ffebe9063cf90c27fee28822b8bdff710a
Adam Palmer <adampalmer@rocketmortgage.com>
2024-10-16 22:03:20 -0400

    Form Builder

--- KajBlogFront/my-angular-app/src/app/components/blog-form-part2/blog-form-part2.component.ts
+++ KajBlogFront/my-angular-app/src/app/components/blog-form-part2/blog-form-part2.component.ts
@@ -1,44 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BlogService } from '../../services/blog.service';
 import { Router } from '@angular/router';
 import { Blog } from '../../models/blog';
 import { SearchComponent } from "../search/search.component";
 import { GifsComponent } from "../gifs/gifs.component";
+import { response } from 'express';
 
 
 @Component({
   selector: 'app-blog-form-part2',
   standalone: true,
-  imports: [CommonModule, FormsModule, SearchComponent, GifsComponent],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './blog-form-part2.component.html',
   styleUrl: './blog-form-part2.component.css'
 })
 export class BlogFormPart2Component {
-
-  blogId: number = 0;
-  userId: string = 'G0f6MzIJs1wjD2GvdzbMdarN4yOMUNIt@clients';
-  category: string = '';
-  subjectLine: string = '';
-  blogBody: string = '';
-  giphyPull: string = '';
+  blogForm: FormGroup;
+  blog: Blog | undefined;
 
 
-
-  constructor(private blogService: BlogService, private router: Router) {}
-
-  createBlog() {
-    var newBlog: Omit<Blog, 'blogId'> = {
-      userId: this.userId,
-      category: this.category,
-      subjectLine: this.subjectLine,
-      blogBody: this.blogBody,
-      giphyPull: this.giphyPull,
-   
-    }
-
+  constructor(private fb: FormBuilder, private blogService: BlogService) {
+    this.blogForm = this.fb.group({
+      category: [''],
+      subjectLine: [''],
+      blogBody: [''],
+    })
   }
+
+  onSubmit() {
+    if (this.blogForm.valid){
+      this.blog = this.blogForm.value as Blog;
+
+      this.blogService.CreateBlog(this.blog).subscribe({
+        next: (response) => {
+          console.log('Blog Succesfully added', response);
+        },
+        error: (err) => {
+          console.error('Error occurred while adding blog', err)
+        }
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
 }
 
 
